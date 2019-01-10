461f5e826981d438046ac7f483e28437e61ec741
nayan <nayan18csu138@ncuindia.edu>
2019-01-09 21:07:16 -0800

    Display Hello

---
+++ DisplayHello.java
@@ -0,0 +1,10 @@
+//printing a line with multiple statements
+//program name must be same as Main class name
+class DisplayHello
+{
+	public static void main {string args[]}
+}
+{ 
+system.out.print("Hello");
+system.out.println("Hello!");
+}
